7ac036f402d1268fd56b9a20ff8cbf388c6e88df
Сергей <polunec@ukr.net>
2019-07-26 12:49:21 +0300

    New list phon

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Сергей Полунец'
+import sys
+import os
+
+
+class Main:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    m = Main()
